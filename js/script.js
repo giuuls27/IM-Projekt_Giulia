aad4ad31040bfe4206794a0d3ce8da41e3065b46
giuuls27 <giulia.buchli@stud.fhgr.ch>
2026-05-20 11:48:24 +0200

    API eingefügt, Startseite gestylet

--- js/script.js
+++ js/script.js
@@ -6,6 +6,46 @@ const place_input = document.querySelector ('#place-input');
 const sun_img = document.querySelector ('#sun-animation');
 
 console.log('DOM Elemente geladen');
-console.log(form_ct);
-console.log(place_input);
-console.log(sun_img)
+
+// 2. Daten-Objekte - daten von API speichern - Startwerte leer, weil werden nach Suche befüllt
+
+let sun_details = {
+    name:'',                    // Name des Orts zb Chur
+    morgendämmerung:'',         // Zeit kommt von API
+    sunrise:'',                 // Aufgangzeit kommt von API
+    sunset:'',                  // Untergangszeit von API
+    abenddämmerung:'',          // Zeit kommt von API
+    timezone: 'Europe/Zurich'   //Zeitzone für korrekte Uhrzeit
+}
+
+console.log ('Daten-Objekt bereit');
+
+//3. API Funktionen
+
+//Funktion 1: Ort suchen und dann Koordinaten (lat/lng) zurück
+
+async function lade_ort(ort) {
+    const url = `https://nominatim.openstreetmap.org/search?city=${ort}&format=json`; 
+    try{
+        const response = await fetch(url);
+        return await response.json();
+    } catch (error){
+        console.error(error);
+        return false;
+    }
+}
+
+//Funktion 2: Sonnenzeit holen - es braucht lat und lng 
+
+async function lade_sonnenzeiten(lat, lng){
+    const url = `https://api.sunrise-sunset.org/json?lat=${lat}&lng=${lng}&formatted=0`;
+    try{
+        const response = await fetch(url);
+        return await response.json();
+    } catch (error) {
+        console.error(error);
+        return false;
+    }
+}
+console.log('API Funktionen bereit')
+
