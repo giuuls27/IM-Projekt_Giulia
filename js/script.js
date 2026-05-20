60240ff1b31c074f43a9b8404959042ddc2a0e3b
giuuls27 <giulia.buchli@stud.fhgr.ch>
2026-05-20 11:03:13 +0200

    Grundstruktur - Start page

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// 1. Dom Elemente
+// Mit querySelector die HTML-Elemente vorstellen
+
+const form_ct = document.querySelector ('#searchform');
+const place_input = document.querySelector ('#place-input');
+const sun_img = document.querySelector ('#sun-animation');
+
+console.log('DOM Elemente geladen');
+console.log(form_ct);
+console.log(place_input);
+console.log(sun_img)
